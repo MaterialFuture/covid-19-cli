5564c429a5cbbf6426fb7a9e368e817fbea20946
Konstantine <konstantinev@protonmail.com>
2020-04-27 14:32:48 -0500

    updated api url and reduced filesize

--- corona-update.sh
+++ corona-update.sh
@@ -7,13 +7,8 @@
 read -r -p "Download the latest Covid-19 news? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
-	curl https://corona.lmao.ninja/countries/USA > ~/.cache/corona-usa
+	curl https://corona.lmao.ninja/v2/countries/USA > ~/.cache/corona-usa
         ;;
     *)
         ;;
 esac
-covid_cases=`cat ~/.cache/corona-usa|jq '.cases'`
-covid_deaths=`cat ~/.cache/corona-usa|jq '.deaths'`
-covid_recovered=`cat ~/.cache/corona-usa|jq '.recovered'`
-#cases_deaths_ratio=`echo="$corona_cases/$corona_deaths"|bc`
-echo -e "The Status of COVID-19 in the US\n😷Cases:\t$covid_cases\n💀Deaths:\t$covid_deaths\n😀Recovered:\t$covid_recovered" > ~/.cache/corona-formatted
